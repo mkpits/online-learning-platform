dd5814d28d36f1a5f07a3650eac76214ac8effc5
Pavan052001 <pavanmohankar80@gmail.com>
2023-10-23 17:17:49 +0530

    adding html pages

--- src/main/java/com/mkpits/learningplatform/controller/HomeController.java
+++ src/main/java/com/mkpits/learningplatform/controller/HomeController.java
@@ -1,14 +1,22 @@
 package com.mkpits.learningplatform.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String homepage(){
-
-        return "/index";
+    public String homepage(Model model){
+        model.addAttribute("tittle","Home -Online learning");
+        return "/home";
     }
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model){
+        model.addAttribute("tittle","about -Online learning");
+        return "/about";
+    }
+
 }
