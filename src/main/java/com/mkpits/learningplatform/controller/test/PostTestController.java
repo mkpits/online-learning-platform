7c3d76317ad0b834cdf125184cd31689b918ab83
Ankita Dudhe <ankitadudhe1996@gmail.com>
2023-11-23 14:50:02 +0530

    get questions on html page from database

--- src/main/java/com/mkpits/learningplatform/controller/test/PostTestController.java
+++ src/main/java/com/mkpits/learningplatform/controller/test/PostTestController.java
@@ -25,32 +25,51 @@ public class PostTestController {
 
         //requesting core java question page
         if (tag.equals("CORE_JAVA")) {
+            // getting list of questions on core java page
             List<Question> questionsList = questionService.getQuestionByTag(tag);
             model.addAttribute("questions",questionsList);
             return "test/core_java";
         }
         //requesting Mysql question page
         else if (tag.equals("MY_SQL")) {
+            // getting list of questions on Mysql page
+            List<Question> questionsList = questionService.getQuestionByTag(tag);
+            model.addAttribute("questions",questionsList);
             return "test/mysql";
         }
         //requesting C question page
         else if (tag.equals("C")) {
+            // getting list of questions on C language page
+            List<Question> questionsList = questionService.getQuestionByTag(tag);
+            model.addAttribute("questions",questionsList);
             return "test/c";
         }
         //requesting C++ question page
         else if (tag.equals("C++")) {
+            // getting list of questions on cpp page
+            List<Question> questionsList = questionService.getQuestionByTag(tag);
+            model.addAttribute("questions",questionsList);
             return "test/cpp";
         }
         //requesting Html question page
         else if (tag.equals("HTML")) {
+            // getting list of questions on html page
+            List<Question> questionsList = questionService.getQuestionByTag(tag);
+            model.addAttribute("questions",questionsList);
             return "test/html";
         }
         //requesting Css question page
         else if (tag.equals("CSS")) {
+            // getting list of questions on css page
+            List<Question> questionsList = questionService.getQuestionByTag(tag);
+            model.addAttribute("questions",questionsList);
             return "test/css";
         }
         //requesting Javascript question page
         else {
+            // getting list of questions on javascript page
+            List<Question> questionsList = questionService.getQuestionByTag(tag);
+            model.addAttribute("questions",questionsList);
             return "test/javascript";
         }
 
