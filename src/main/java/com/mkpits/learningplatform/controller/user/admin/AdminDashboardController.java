cb273d5ed98456cb6638b2612d3b5c9787d7e923
pranayjumde0812 <pranayjumde13@gmail.com>
2023-11-29 17:57:31 +0530

    Dashboard Designing of student and admin (WIP)

--- src/main/java/com/mkpits/learningplatform/controller/user/admin/AdminDashboardController.java
+++ src/main/java/com/mkpits/learningplatform/controller/user/admin/AdminDashboardController.java
@@ -1,14 +1,29 @@
 package com.mkpits.learningplatform.controller.user.admin;
 
+import com.mkpits.learningplatform.model.User;
+import com.mkpits.learningplatform.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class AdminDashboardController {
 
-    @RequestMapping("/admin/index")
-    public String adminLogin() {
+    @Autowired
+    private UserService userService;
 
+    @RequestMapping("/admin/index")
+    public String adminLogin(Model model, Principal principal) {
+
+        String userName = principal.getName();
+
+        User user = userService.getUserByUserName(userName);
+
+        model.addAttribute("user", user);
+        model.addAttribute("title", "Admin - Dashboard");
         return "/admin/admin-dashboard";
     }
 }
