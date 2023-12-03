126cac6206ce56266a646bf677ebe51fd844b15a
pranayjumde0812 <pranayjumde13@gmail.com>
2023-12-03 22:10:44 +0530

    Validation completed of registration form

--- src/main/java/com/mkpits/learningplatform/controller/user/normal/PostUserRegistrationController.java
+++ src/main/java/com/mkpits/learningplatform/controller/user/normal/PostUserRegistrationController.java
@@ -6,11 +6,13 @@ import com.mkpits.learningplatform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 public class PostUserRegistrationController {
@@ -19,16 +21,24 @@ public class PostUserRegistrationController {
     private UserService userService;
 
     @PostMapping("/register")
-    public String registerNewUser(@ModelAttribute("user") User user,
+    public String registerNewUser(@Valid @ModelAttribute("user") User user,
+                                  BindingResult bindingResult,
                                   Model model,
                                   @RequestParam(value = "agreement",
                                           defaultValue = "false") boolean agreement,
                                   HttpSession session) {
 
         try {
+            // Agreement check
             if (!agreement) {
                 System.out.println("You have not agreed the terms and conditions");
                 throw new Exception("You have not agreed the terms and conditions");
+            }
+
+            // Validation Check
+            if (bindingResult.hasErrors()) {
+                model.addAttribute("user", user);
+                return "user-signup";
             }
 
             User saveUser = userService.registerNewUser(user);
