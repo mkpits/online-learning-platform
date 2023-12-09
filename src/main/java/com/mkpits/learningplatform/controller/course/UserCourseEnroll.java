720e351fdc5d7e365ad0a7f0daab9f6d0ceb2fff
Pavan052001 <pavanmohankar80@gmail.com>
2023-12-09 16:54:25 +0530

    UserEnroll controller

---
+++ src/main/java/com/mkpits/learningplatform/controller/course/UserCourseEnroll.java
@@ -0,0 +1,14 @@
+package com.mkpits.learningplatform.controller.course;
+
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+public class UserCourseEnroll {
+
+    public String enrollCourse(){
+
+        return null;
+    }
+}
